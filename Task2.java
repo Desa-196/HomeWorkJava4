f234245dc11137a9b7ffd62d7685f7d0cc8e2e8d
Денис Пономарев <deskin196@gmail.com>
2023-07-04 14:31:53 +0300

    Добавил свой класс очереди и написал пример работы

--- Task2.java
+++ Task2.java
@@ -5,5 +5,27 @@ dequeue() - возвращает первый элемент из очереди
 public class Task2 {
     public static void main(String[] args){
 
+        MyQueue myQueue = new MyQueue();
+
+        //Добавляем элементы в коней очереди
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        myQueue.enqueue(4);
+
+        System.out.println(myQueue);
+        System.out.print("Первый элемент с удалением: ");
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue);
+        System.out.print("Первый элемент без удаления: ");
+        System.out.println(myQueue.first());
+        System.out.println(myQueue);
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        //При пустой очереди будет возвращать null
+        System.out.println(myQueue.dequeue());
+
+
     }
 }
